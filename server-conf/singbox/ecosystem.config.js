8d6f60f039d8c50561fd4a29513324a20f3ae249
Eric Moore <EricGittaMoore@duck.com>
2025-12-08 17:22:45 +0800

    up

--- server-conf/singbox/ecosystem.config.js
+++ server-conf/singbox/ecosystem.config.js
@@ -1,3 +1,23 @@
+/*
+# 安装 singbox
+  $ curl https://github.com/SagerNet/sing-box/releases/download/v1.12.12/sing-box-1.12.12-linux-amd64.tar.gz -L | tar xzvf -
+
+# 给 singbox 使用前需要转 json
+  $ yq -o j 'del(.comment)' config.yaml > config.json
+
+# pm2 保活
+  $ apk add npm && npm i -g pm2 @dotenvx/dotenvx
+# pm2 开机自动启动
+  $ pm2 startup
+# pm2 日志插件，限制日志大小，默认 10MB
+  $ pm2 install pm2-logrotate
+# pm2 运行应用
+  $ pm2 start -n caddy dotenvx -- run -- caddy run --environ
+  $ pm2 start -n singbox ./sing-box -- run -D .
+  $ pm2 start -n masque ./usque -- socks -b 127.0.0.1 -p 1080
+# 最后保存一下配置
+  $ pm2 save
+*/
 require("@dotenvx/dotenvx").config();
 
 const cwd = __dirname;
